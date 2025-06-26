09c0cd1dace9397d8baa0e9067d6ade159fc1774
LeonardoDavidRY <ldramirez@uce.edu.ec>
2025-06-25 19:52:04 -0500

    Tarea 8

--- src/main/java/uce/edu/web/api/controller/ProfesorController.java
+++ src/main/java/uce/edu/web/api/controller/ProfesorController.java
@@ -1,5 +1,7 @@
 package uce.edu.web.api.controller;
 
+import java.util.List;
+
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -14,9 +16,15 @@ public class ProfesorController {
     private IProfesorService profesorService;
 
     @GET
-    @Path("/consultar/{id}")
+    @Path("/{id}")
     public Profesor consultarPorId(@PathParam("id") Integer id) {
         return this.profesorService.buscarPorId(id);
+    }
+
+    @GET
+    @Path("")
+    public List<Profesor> consultarTodos() {
+        return this.profesorService.buscarTodos();
     }
 
 }
